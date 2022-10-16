db0ebf9a15abd45ca1440dc9fb03a1cf381f2410
fer <ferp@protonmail.ch>
2022-10-16 19:07:27 +0200

    removed unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Map from "./components/Map";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // fetch data from free weather API for Spain
-
   return (
     <div className="App">
+      <h1>Basic Map</h1>
       <Map />
-      <p>Basic Map</p>
     </div>
   );
 }
